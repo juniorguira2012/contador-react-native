e447f0ba9046694955076b3f92596eaea542ee63
Junior Done <juniorguira2012@gmail.com>
2021-02-04 00:51:41 -0400

    commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { useState } from 'react'
-import {Text,View,Button,} from 'react-native'
+import {Text,View,Button,StyleSheet} from 'react-native'
 
 const Contador = () =>{
   const [contador, setContador] = useState(0)
@@ -18,17 +18,45 @@ const Contador = () =>{
   }
 
   return(
-      <View>
-        <View>
-          <Text>{contador}</Text>
-
+      <View style={styles.container}>
+        <View style={styles.boxview}>
+          <Text style={styles.contador}>{contador}</Text>
         </View>
-        <Button
-            title='Contando'
-            onPress={incremental}
-          />     
+        <View style={styles.button}>
+          <Button
+              title='INICIALIZAR CONTEO'
+              onPress={incremental}
+            />     
+        </View>    
       </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: "space-between",
+    backgroundColor: `darkgrey`,
+    padding: 20,
+    margin: 10,
+  },
+  boxview:{
+    flex: 1,
+    backgroundColor: "burlywood",
+    borderWidth: 5,
+  
+  },
+  button:{
+    flex:1,
+    justifyContent:'center',
+  },
+
+  contador:{
+    fontSize:90,
+    position:'absolute',
+    top:100,
+    left:115,
+  }
+});
 
 export default Contador;
